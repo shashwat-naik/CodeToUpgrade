b28a57aeab2caef883a4c3f82b283183fb52a519
shashwat-naik <naikshashwat3@gmail.com>
2025-02-07 22:18:20 +0530

    Max Consecutive 1 in array

---
+++ Arrays/Max_ConsecutiveOne.java
@@ -0,0 +1,20 @@
+public class Max_ConsecutiveOne {
+    public static int maxConsecutiveOne(int[] arr, int n) {
+        int maximum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1)
+                count++;
+            else
+                count = 0;
+
+            maximum = Math.max(count, maximum);
+        }
+        return maximum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 0, 1, 1, 1 };
+        System.out.println("Max num of consecutive 1 present is " + maxConsecutiveOne(arr, arr.length));
+    }
+}
