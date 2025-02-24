88620e56c819984b2d71944851c5bb08fda21ec0
shashwat-naik <naikshashwat3@gmail.com>
2025-02-24 10:23:42 +0530

    Merge Sorted LL

---
+++ LinkedList/Merge_SortedLL.java
@@ -0,0 +1,69 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = null;
+    }
+}
+
+public class Merge_SortedLL {
+    public static Node mergeLL(Node list1, Node list2) {
+        Node storeLL = new Node(-1);
+        Node temp = storeLL;
+
+        while (list1 != null && list2 != null) {
+            if (list1.data <= list2.data) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (list1 != null)
+            temp.next = list1;
+        else
+            temp.next = list2;
+
+        return storeLL.next;
+    }
+
+    public static void printLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node list1 = new Node(1);
+        list1.next = new Node(3);
+        list1.next.next = new Node(5);
+
+        Node list2 = new Node(2);
+        list2.next = new Node(4);
+        list2.next.next = new Node(6);
+
+        System.out.print("First sorted linked list: ");
+        printLinkedList(list1);
+
+        System.out.print("Second sorted linked list: ");
+        printLinkedList(list2);
+
+        Node mergedList = mergeLL(list1, list2);
+
+        System.out.print("Merged sorted linked list: ");
+        printLinkedList(mergedList);
+    }
+}
--- LinkedList/Middle_elem_LL.java
+++ LinkedList/Middle_elem_LL.java
@@ -21,7 +21,7 @@ public class Middle_elem_LL {
         while (slow != null && fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-        }
+        } 
         return slow;
     }
 
